0285127761840b5a9e1355b44642c79ac1efa796
quziwei <quziwei@1379010735507589.onaliyun.com>
2020-09-29 12:01:16 +0800

    code-docsite

---
+++ site_config/home.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+
+export default {
+  'zh-cn': {
+    brand: {
+      brandName: 'C O D E',
+      briefIntroduction: 'CODE开源项目为了学习和快速使用。如果您是初学者，可以使用此项目快速构建您的WEB应用',
+      buttons: [
+        {
+          text: '立即开始',
+          link: '/zh-cn/docs/demo1.html',
+          type: 'primary',
+        },
+        {
+          text: '查看Github',
+          link: 'https://github.com/YueGod/',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: '开源产品的介绍',
+      desc: '开源产品的简单介绍，提供一些该产品的优点、特性描述等',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: '特性一览',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: '特性1',
+          content: '体积小',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: '特性2',
+          content: '开发快速',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: '特性3',
+          content: '解决方案',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: '特性4',
+          content: '企业级',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: '特性5',
+          content: '学习性',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: '特性6',
+          content: '全部开源',
+        },
+      ],
+    },
+    start: {
+      title: '快速开始',
+      desc: '简单描述',
+      img: '/img/quick_start.png',
+      button: {
+        text: '阅读更多',
+        link: '/zh-cn/docs/demo1.html',
+      },
+    },
+    users: {
+      title: '用户',
+      desc: <span>简单描述</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+  'en-us': {
+    brand: {
+      brandName: 'C O D E',
+      briefIntroduction: 'The CODE open source project is for learning and quick use. If you are a beginner, you can use this project to quickly build your WEB application',
+      buttons: [
+        {
+          text: 'Quick Start',
+          link: '/en-us/docs/demo1.html',
+          type: 'primary',
+        },
+        {
+          text: 'View on Github',
+          link: 'https://github.com/YueGod/',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: 'introduction title',
+      desc: 'some introduction of your product',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: 'Feature List',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: 'feature1',
+          content: 'small volume',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: 'feature2',
+          content: 'Fast development',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: 'feature3',
+          content: 'solution',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: 'feature4',
+          content: 'Enterprise',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: 'feature5',
+          content: 'Learning',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: 'feature6',
+          content: 'All open source',
+        }
+      ]
+    },
+    start: {
+      title: 'Quick start',
+      desc: 'some description text',
+      img: '/img/quick_start.png',
+      button: {
+        text: 'READ MORE',
+        link: '/en-us/docs/demo1.html',
+      },
+    },
+    users: {
+      title: 'users',
+      desc: <span>some description</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+};
